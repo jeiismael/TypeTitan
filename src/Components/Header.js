d75af72014045a44007d88fdd27bce7c61c4fa08
jeiismael <jeiismael@gmail.com>
2023-09-26 22:55:07 +0800

    added files from home PC: loginpage, edited code on TypeBox

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Type Titan</Navbar.Brand>
+        <Navbar.Brand href="#home">TypeTitan</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/Components/TypeBox.js
+++ src/Components/TypeBox.js
@@ -3,29 +3,32 @@ const TypeBox = () => {
     const quoteDisplayElement = document.getElementById('quoteDisplay')
     const quoteInputElement = document.getElementById('quoteInput')
     
-    quoteInputElement.addEventListener('input', () => {
-      const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-      const arrayValue = quoteInputElement.value.split('')
-    
-      let correct = true
-      arrayQuote.forEach((characterSpan, index) => {
-        const character = arrayValue[index]
-        if (character == null) {
-          characterSpan.classList.remove('correct')
-          characterSpan.classList.remove('incorrect')
-          correct = false
-        } else if (character === characterSpan.innerText) {
-          characterSpan.classList.add('correct')
-          characterSpan.classList.remove('incorrect')
-        } else {
-          characterSpan.classList.remove('correct')
-          characterSpan.classList.add('incorrect')
-          correct = false
-        }
+    if (quoteInputElement !== null) {
+      quoteInputElement.addEventListener('input', () => {
+        const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+        const arrayValue = quoteInputElement.value.split('')
+      
+        let correct = true
+        arrayQuote.forEach((characterSpan, index) => {
+          const character = arrayValue[index]
+          if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+          } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+          } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+          }
+        })
+      
+        if (correct) renderNewQuote()
       })
+    }
     
-      if (correct) renderNewQuote()
-    })
 
     function getRandomQuote() {
         return fetch(RANDOM_QUOTE_API_URL)
